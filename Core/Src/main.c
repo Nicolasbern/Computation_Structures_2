5227f1bf50e774339dd5770d5f77f85bb082b173
Nicolasbern <115322460+Nicolasbern@users.noreply.github.com>
2023-10-08 12:27:45 -0500

    feat(rtc): adding the RTC and an alarm in interrupt mode.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "stdio.h"
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -41,6 +41,9 @@
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
+
+RTC_HandleTypeDef hrtc;
 
 TIM_HandleTypeDef htim1;
 TIM_HandleTypeDef htim7;
@@ -49,7 +52,11 @@ UART_HandleTypeDef huart2;
 DMA_HandleTypeDef hdma_usart2_tx;
 
 /* USER CODE BEGIN PV */
+uint8_t rx_buffer[4];
 
+uint8_t tx_busy = 0;
+
+uint16_t adc_buffer[10];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -60,11 +67,8 @@ static void MX_USART2_UART_Init(void);
 static void MX_TIM7_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_ADC1_Init(void);
+static void MX_RTC_Init(void);
 /* USER CODE BEGIN PFP */
-
-uint8_t Rx_data[4];
-
-uint8_t Tx_busy = 0;
 
 /* USER CODE END PFP */
 
@@ -72,28 +76,66 @@ uint8_t Tx_busy = 0;
 /* USER CODE BEGIN 0 */
 int _write(int file, char *ptr, int len)
 {
-	while (Tx_busy != 0) {/* Wait */}
+	while (tx_busy != 0) {/* Wait */}
   HAL_UART_Transmit_DMA(&huart2, (uint8_t *)ptr, len);
-  Tx_busy = 1;
+  tx_busy = 1;
   return len;
 }
+
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-	printf("Received: [%s]\r\n", Rx_data);
-	HAL_UART_Receive_IT(&huart2, Rx_data, 1);
+	printf("Received: [%s]\r\n", rx_buffer);
+	HAL_UART_Receive_IT(&huart2, rx_buffer, 1);
 }
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
 {
-	Tx_busy = 0;
+	tx_busy = 0;
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
 	static uint32_t counter = 0;
-	printf("Counter: %ld\r\n",counter++);
+
+	printf("Counter: %ld\r\n", counter++);
 }
 
+static void rtc_alarm_config(void)
+{
+	RTC_DateTypeDef date;
+	RTC_TimeTypeDef time;
+	RTC_AlarmTypeDef alarm;
+
+	date.Year = 0x23;
+	date.Month = RTC_MONTH_OCTOBER;
+	date.Date = 0x06;
+	date.WeekDay = RTC_WEEKDAY_FRIDAY;
+	HAL_RTC_SetDate(&hrtc, &date, RTC_FORMAT_BCD);
+
+	time.Hours = 0x19;
+	time.Minutes = 0x10;
+	time.Seconds = 0x00;
+	time.TimeFormat = RTC_HOURFORMAT_24;
+	HAL_RTC_SetTime(&hrtc, &time, RTC_FORMAT_BCD);
+
+	alarm.Alarm = RTC_ALARM_A;
+	alarm.AlarmTime.Hours = 0x19;
+	alarm.AlarmTime.Minutes = 0x10;
+	alarm.AlarmTime.Seconds = 0x10;
+
+	alarm.AlarmTime.TimeFormat = RTC_HOURFORMAT_24;
+	alarm.AlarmDateWeekDay = RTC_WEEKDAY_FRIDAY;
+	alarm.AlarmDateWeekDaySel = RTC_ALARMDATEWEEKDAYSEL_WEEKDAY;
+	alarm.AlarmMask = RTC_ALARMMASK_DATEWEEKDAY;
+	HAL_RTC_SetAlarm_IT(&hrtc, &alarm, RTC_FORMAT_BCD);
+
+}
+
+void HAL_RTC_AlarmAEventCallback(RTC_HandleTypeDef *hrtc)
+{
+  /* Turn LD2 on: Alarm generation */
+  printf("Alarm!!!\r\n");
+}
 /* USER CODE END 0 */
 
 /**
@@ -129,41 +171,34 @@ int main(void)
   MX_TIM7_Init();
   MX_TIM1_Init();
   MX_ADC1_Init();
-
+  MX_RTC_Init();
   /* USER CODE BEGIN 2 */
-  HAL_UART_Receive_IT(&huart2, Rx_data, 4);
+  HAL_UART_Receive_IT(&huart2, rx_buffer, 1);
 
   HAL_TIM_Base_Start_IT(&htim7);
 
-  TIM1->CCR1 = 50;
+  TIM1->CCR1 = 50; // To set the compare channel 1
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
- // printf("Hello World! /r/n");
-
- for(uint8_t idx = 0; idx <0x0F;idx++)
-	  printf("IDX: 0x%02X\r\n",idx);
-
-
+  rtc_alarm_config();
   while (1)
   {
+	  HAL_ADC_Start_DMA(&hadc1, (uint32_t *)adc_buffer, 10);
+	  HAL_Delay(1000);
+	  uint32_t adc_channel_1 = adc_buffer[0] + adc_buffer[2] +
+			  adc_buffer[4] + adc_buffer[6] + adc_buffer[8];
+	  adc_channel_1 = adc_channel_1 / 5;
+	  uint32_t adc_channel_2 = adc_buffer[1] + adc_buffer[3] +
+	  			  adc_buffer[5] + adc_buffer[7] + adc_buffer[9];
+	  	  adc_channel_2 = adc_channel_2 / 5;
+	  printf("Average: %ld, %ld\r\n", adc_channel_1, adc_channel_2);
+
     /* USER CODE END WHILE */
-	 HAL_ADC_Start(&hadc1);
-	 HAL_ADC_PollForConversion(&hadc1, 10);
-	 uint16_t adc_value = HAL_ADC_GetValue(&hadc1);
-	 HAL_ADC_PollForConversion(&hadc1, 10);
-	 uint16_t adc_value2 = HAL_ADC_GetValue(&hadc1);
-	 float adc_voltage = (adc_value/4096.0 * 5.5);
-	 float adc_voltage2 = (adc_value2/4096.0 * 5.5);
-	 printf("ADC Reading %f,  %f\r\n", adc_voltage, adc_voltage2);
-	 HAL_Delay(100);
 
     /* USER CODE BEGIN 3 */
-
-
   }
   /* USER CODE END 3 */
 }
@@ -187,9 +222,10 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLM = 1;
@@ -243,7 +279,7 @@ static void MX_ADC1_Init(void)
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
   hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
-  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc1.Init.EOCSelection = ADC_EOC_SEQ_CONV;
   hadc1.Init.LowPowerAutoWait = DISABLE;
   hadc1.Init.ContinuousConvMode = ENABLE;
   hadc1.Init.NbrOfConversion = 2;
@@ -294,6 +330,89 @@ static void MX_ADC1_Init(void)
 }
 
 /**
+  * @brief RTC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_RTC_Init(void)
+{
+
+  /* USER CODE BEGIN RTC_Init 0 */
+
+  /* USER CODE END RTC_Init 0 */
+
+  RTC_TimeTypeDef sTime = {0};
+  RTC_DateTypeDef sDate = {0};
+  RTC_AlarmTypeDef sAlarm = {0};
+
+  /* USER CODE BEGIN RTC_Init 1 */
+
+  /* USER CODE END RTC_Init 1 */
+
+  /** Initialize RTC Only
+  */
+  hrtc.Instance = RTC;
+  hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
+  hrtc.Init.AsynchPrediv = 127;
+  hrtc.Init.SynchPrediv = 255;
+  hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
+  hrtc.Init.OutPutRemap = RTC_OUTPUT_REMAP_NONE;
+  hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+  hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+  if (HAL_RTC_Init(&hrtc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN Check_RTC_BKUP */
+
+  /* USER CODE END Check_RTC_BKUP */
+
+  /** Initialize RTC and set the Time and Date
+  */
+  sTime.Hours = 0x0;
+  sTime.Minutes = 0x0;
+  sTime.Seconds = 0x0;
+  sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+  sTime.StoreOperation = RTC_STOREOPERATION_RESET;
+  if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BCD) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sDate.WeekDay = RTC_WEEKDAY_MONDAY;
+  sDate.Month = RTC_MONTH_JANUARY;
+  sDate.Date = 0x1;
+  sDate.Year = 0x0;
+
+  if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BCD) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Enable the Alarm A
+  */
+  sAlarm.AlarmTime.Hours = 0x0;
+  sAlarm.AlarmTime.Minutes = 0x0;
+  sAlarm.AlarmTime.Seconds = 0x0;
+  sAlarm.AlarmTime.SubSeconds = 0x0;
+  sAlarm.AlarmTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+  sAlarm.AlarmTime.StoreOperation = RTC_STOREOPERATION_RESET;
+  sAlarm.AlarmMask = RTC_ALARMMASK_NONE;
+  sAlarm.AlarmSubSecondMask = RTC_ALARMSUBSECONDMASK_ALL;
+  sAlarm.AlarmDateWeekDaySel = RTC_ALARMDATEWEEKDAYSEL_DATE;
+  sAlarm.AlarmDateWeekDay = 0x1;
+  sAlarm.Alarm = RTC_ALARM_A;
+  if (HAL_RTC_SetAlarm_IT(&hrtc, &sAlarm, RTC_FORMAT_BCD) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN RTC_Init 2 */
+
+  /* USER CODE END RTC_Init 2 */
+
+}
+
+/**
   * @brief TIM1 Initialization Function
   * @param None
   * @retval None
@@ -314,7 +433,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 8001;
+  htim1.Init.Prescaler = 800-1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 100-1;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -456,6 +575,9 @@ static void MX_DMA_Init(void)
   __HAL_RCC_DMA1_CLK_ENABLE();
 
   /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
   /* DMA1_Channel7_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel7_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel7_IRQn);
@@ -475,14 +597,17 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
-  /*Configure GPIO pin : PC13 */
+  /*Configure GPIO pin : USER_BUTTON_Pin */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+   GPIO_InitStruct.Pull = GPIO_NOPULL;
+   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
